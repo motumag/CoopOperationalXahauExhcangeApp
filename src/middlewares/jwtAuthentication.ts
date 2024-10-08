65a12505a07c6c56bcae0c28f970621bbe16a448
motumag <motumagishu27@gmail.com>
2024-10-09 00:28:36 +0300

    jwt authenticatin clearance

--- src/middlewares/jwtAuthentication.ts
+++ src/middlewares/jwtAuthentication.ts
@@ -1,21 +1,3 @@
-// import { Request, Response, NextFunction } from 'express';
-// import jwt from 'jsonwebtoken';
-
-// export const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
-//   const token = req.header('Authorization')?.split(' ')[1];
-
-//   if (!token) {
-//     return res.status(401).json({ message: 'Access denied. No token provided.' });
-//   }
-//   try {
-//     const secretKey = process.env.JWT_SECRET || '2371974832747978374';
-//     const decoded = jwt.verify(token, secretKey); 
-//     req.user = decoded;  
-//     next(); 
-//   } catch (error) {
-//     return res.status(400).json({ message: 'Invalid token.' });
-//   }
-// };
 import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 
